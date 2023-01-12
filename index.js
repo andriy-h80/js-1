aaaba8c1399e81a40d2810309f9c511f68c5dc6b
KOSTIANTYN SHELKOVENKO <kos8704@ukr.net>
2023-01-12 21:49:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,13 @@
 // const message = answer === "ECMAScript".toLowerCase() ? "True!" : "Don't you know? ECMAScript!";
 // alert(message);
 
+
+//2. Напишіть програму, яка запитає у користувача через prompt "Enter the number of minutes to format"
+//число (кількість хвилин) і виведе в консоль
+//рядок у форматі годин і хвилин, тобто 70 має повернути у консоль "01:10"
+
+
+// const answer = prompt("Enter the number of minutes to format");
+// const hours =  Math.floor(answer / 60);
+// const minutes = answer % 60;
+// console.log(`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`);
